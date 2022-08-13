3864116733615780fcc373fae808ce8e87601c18
Jared Wasinger <j-wasinger@hotmail.com>
2022-08-13 18:48:53 +0000

    update

--- db.py
+++ db.py
@@ -1,16 +1,46 @@
+import sqlite3
+import os
+
+DB_NAME='precompiles.db'
 
 class DB():
-    def create_new_db():
+    def __init__(self):
         pass
 
-    def open_existing_db():
-        pass
-
+    @staticmethod
     def create_or_new():
-        pass
+        already_exists = False
+        if os.path.exists(DB_NAME):
+            already_exists = True
 
-    def SetHeadBlock(self, number: int, hsh: str):
-        pass
+        db = DB()
+        db.connection = sqlite3.connect(DB_NAME)
+        db.cursor = db.connection.cursor()
+
+        if not already_exists:
+            print("creating new db")
+            db.createHeadBlockTable()
+            db.createCallTable()
+            db.SetHeadBlockNumber(0)
+        else:
+            print("opened existing db")
+
+        return db
+
+    def createCallTable(self):
+        self.cursor.execute('CREATE TABLE CALLS(to TEXT, from TEXT, input TEXT, gasUsed TEXT)')
+
+    def createHeadBlockTable(self):
+        self.cursor.execute('CREATE TABLE HEADBLOCK(id TEXT NOT NULL, number TEXT NOT NULL, PRIMARY KEY (id) )')
+
+    def SetHeadBlockNumber(self, number: int):
+        self.head_number = number
+        self.cursor.execute('INSERT INTO HEADBLOCK ("headblock", {})'.format(number))
+        # TODO self.cursor.execute('....')
+
+    def HeadBlockNumber(self):
+        result = self.cursor.execute('SELECT * from HEADBLOCK')
+        import pdb; pdb.set_trace()
 
     def SetPrecompileCall(self, block_number: int, tx_hash: str, to: str, input_data: str, result: str, gas_used: int):
         pass
--- query.py
+++ query.py
@@ -1,36 +1,41 @@
-from eth_jsonrpc_ws import ETHRPCClient
+from db import DB
+
+from eth_jsonrpc_ws import EthRPCClient
 import sqlite3
+import asyncio
 
 connection = sqlite3.connect('precompile_calls.db')
+WS_RPC_URL='ws://localhost:8546'
 
 cursor = connection.cursor()
 # query to see if table exists:
 # SELECT name FROM sqlite_master WHERE type='table' AND name='{table_name}';
 
-def db_exists():
-    pass
-
-def run_collection(db):
-    head = rpc.GetHeadBlockNumber()
-    local_head = db.HeadBlock().Number
-    if head > local_head:
-        print("local head block nr {} greater than remote block nr {}. nothing to collect")
-        return
-
-    # for each block number in range(local_head + 1, remote_head + 1):
-        # for each tx in block:
-            # trace = ....
-            # calls = extract_precompile_calls(trace)
-            # for call in calls:
-            #   db.SetPrecompileCall(...)
-        # db.SetHeadBlock(block number)
-
-def main():
-    async with ETHRPCClient as rpc:
+async def run_collection():
+    rpc = EthRPCClient(WS_RPC_URL)
+    async with rpc:
         db = DB.create_or_new()
 
         while True:
-            run_collection(db)
+            remote_head = rpc.GetHeadBlockNumber()
+            local_head = db.HeadBlockNumber()
+            if remote_head > local_head:
+                print("local head block nr {} greater than remote block nr {}. nothing to collect")
+                return
+
+            # for each block number in range(local_head + 1, remote_head + 1):
+                # for each tx in block:
+                    # trace = ....
+                    # calls = extract_precompile_calls(trace)
+                    # for call in calls:
+                    #   db.SetPrecompileCall(...)
+                # db.SetHeadBlock(block number)
+
+            #for block in range(local_head + 1, remote_head + 1):
+            #    pass
+
+def main():
+    asyncio.get_event_loop().run_until_complete(run_collection())
 
 if __name__ == "__main__":
     main()
